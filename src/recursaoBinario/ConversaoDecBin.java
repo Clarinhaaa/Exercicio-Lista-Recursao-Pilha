1c05e1db7e93ec5ab17a643e07da1b3ffc34338f
Ana Clara <anaclara.cavalcante.2007@gmail.com>
2024-09-17 12:02:33 -0300

    Mudanças na conversão de decimal para binário

--- src/recursaoBinario/ConversaoDecBin.java
+++ src/recursaoBinario/ConversaoDecBin.java
@@ -2,14 +2,8 @@ package recursaoBinario;
 
 public class ConversaoDecBin {
     public String converter(int numDec) {
-        if (numDec == 2) {
-            return "10";
-        }
-        
-        if (numDec == 3) {
-            return "11";
-        }
-
-        return this.converter(numDec / 2) + numDec % 2;
+        if (numDec > 0)
+            return this.converter(numDec / 2) + numDec % 2;
+        return "";
     }
 }
